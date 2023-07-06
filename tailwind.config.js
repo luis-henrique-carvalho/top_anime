a65272eaeb8c759248cef2bf79db5905b2ebce10
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2023-07-06 20:55:10 -0300

    :wrench: chore: initial setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
+import tailwindScrolblar from "tailwind-scrollbar";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "anime-blue": "#00A3FF",
+        "anime-white": "#F9F9F9",
+        "anime-gray": "#BFBFBF",
+        "anime-dark": "#06090F",
+        "anime-dark-200": "#151B26",
+      },
+      scrollbar: {
+        width: "0.3rem",
+        height: "0.4rem",
+        track: "151B26",
+        thumb: "#00A3FF",
       },
     },
   },
-  plugins: [],
-}
+  variants: {
+    scrollbar: ["dark"], // Adicione a variante 'dark' para lidar com o tema escuro (opcional)
+  },
+  plugins: [
+    tailwindScrolblar, // Importe o plugin do tailwind-scrollbar
+  ],
+};
