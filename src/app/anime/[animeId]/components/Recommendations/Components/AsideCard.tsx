fc607fdae525463918c8c9d27f17a20d1edf6376
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-07-28 21:10:28 -0300

    :sparkles: feat: added AsideCard Component

---
+++ src/app/anime/[animeId]/components/Recommendations/Components/AsideCard.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type Props = {
+  image: string;
+  id: string;
+  title: string;
+  index: number;
+};
+
+const AsideCard = ({ id, image, title, index }: Props) => {
+  return (
+    <Link
+      href={`/anime/${id}`}
+      className={`flex flex-row pointer-events-auto mb-[14px] h-28 justify-between bg-anime-dark-200`}
+      key={index}
+    >
+      <div className="relative w-[100px] h-28">
+        <Image
+          src={image}
+          fill
+          priority
+          alt="Picture of the author"
+          sizes="(max-width: 640px) 100vw, 640px" // Adicione essa linha com o tamanho esperado da imagem
+        />
+      </div>
+
+      <div className="text-white flex flex-col w-3/5 justify-center p-2">
+        <h4 className="line-clamp-2 text-sm font-medium">{title}</h4>
+        <p className="text-anime-gray">{id}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default AsideCard;
