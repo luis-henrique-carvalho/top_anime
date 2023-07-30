f7a8579fb30f0d497987a9adb43dbcb0a4d01191
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-07-30 13:53:55 -0300

    :lipstick: feat: adjusted Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React from "react";
-import { redirect, useSearchParams } from "next/navigation";
+import { redirect, usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 interface Props {
@@ -7,13 +8,18 @@ interface Props {
 }
 
 const Button: React.FC<Props> = ({ value }) => {
-  // const searchParams = useSearchParams()
-  // const search = searchParams.get('/?q=')
+  const searchParams = usePathname().replace("/search/", "");
+
+  console.log(searchParams);
 
   return (
     <Link
-      href={`/search?q=${value}`}
-      className="text-anime-white inline-flex items-center justify-center w-20 md:w-32 text-xs md:text-lg h-10 text-center bg-transparent border-[0.1rem] border-solid border-anime-dark-200 rounded-full leading-10 transition-all duration-300 select-none"
+      href={value === "Todos" ? "/" : `/search/${value}`}
+      className={`text-anime-white ${
+        searchParams === "/" && value === "Todos" ? "bg-[#151B26]" : ""
+      } ${
+        searchParams === value ? "bg-[#151B26]" : ""
+      } inline-flex items-center justify-center w-20 md:w-32 text-xs md:text-lg h-10 text-center bg-transparent border-[0.1rem] border-solid border-anime-dark-200 rounded-full leading-10 transition-all duration-300 select-none`}
     >
       {value}
     </Link>
